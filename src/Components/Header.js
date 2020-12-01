3d0ebfd6f81e46e6aefd689013b43f9bff2aa5bf
Paul Rogers <progers999@hotmail.co.uk>
2020-12-01 14:35:14 +0000

    Header markup complete

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header>
+			<i className="fa fa-github"></i>
+			<h1>C.V. Builder</h1>
+			<i className="fa fa-address-card"></i>
+		</header>
+	)
+
+}
+
+export default Header
