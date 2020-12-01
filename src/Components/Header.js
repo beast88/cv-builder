0b1861538192946a88dfe2afe5634d484bc183e5
Paul Rogers <progers999@hotmail.co.uk>
2020-12-01 14:54:26 +0000

    Styles have been added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header>
 			<i className="fa fa-github"></i>
-			<h1>C.V. Builder</h1>
+			<h1 className="title">C.V. Builder</h1>
 			<i className="fa fa-address-card"></i>
 		</header>
 	)
