ec6172f9ec0245646276eeb31d41ee29b6fe60fc
Paul Rogers <progers999@hotmail.co.uk>
2020-12-01 15:34:49 +0000

    Resume component added and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
+import Resume from './Components/Resume'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Resume />
     </div>
   )
 
---
+++ src/Components/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+	return (
+		<main>
+			<h1>Build some stuff here</h1>
+		</main>
+	)
+}
+
+export default Resume
