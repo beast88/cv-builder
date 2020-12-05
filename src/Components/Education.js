2650ae9f2e75f077889dcd4739aeff623fbea41e
Paul Rogers <progers999@hotmail.co.uk>
2020-12-05 10:47:47 +0000

    Values from user in the education form are saved in state

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -4,29 +4,67 @@ class Education extends Component{
 	constructor(){
 		super()
 		this.state = {
-			education: [{
-				id: 1,
+			default: {
+				id: '',
 				school: '',
 				from: '',
 				to: '',
 				qualification: '',
 				grade: ''
-			}]
+			},
+			activeForm: false,
+			activeButton: true,
+			quals: []
 		}
 	}
 
+	toggleForm = () => {
+		this.setState({
+			activeForm: !this.state.activeForm,
+			activeButton: !this.state.activeButton
+		})
+	}
+
+	handleAdd = () => {
+		this.toggleForm()
+	}
+
+	handleChange = (e) => {
+		const {name, value} = e.target
+		
+		this.setState({
+			default: {...this.state.default, [name]: value}
+		})
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		//Need to move the value of default into quals array and reset default and toggle form
+	}
+
+	handleCancel = (e) => {
+		e.preventDefault()
+		this.toggleForm()
+	}
+
 	render(){
+
+		let hiddenForm = this.state.activeForm ? "education-form" : "education-form hidden"
+		let hiddenButton = this.state.activeButton ? "add-item" : "add-item hidden"
+
 		return(
 			<div className="education">
 				<h3>Education</h3>
 
-				<form className="education-form hidden">
+				<form className={hiddenForm} onSubmit={this.handleSubmit}>
 					<div className="form-control">
 						<label for="school">Name of University or School:</label>
 						<input
 							type="text"
 							name="school"
-							placeholder="Enter Here" 
+							placeholder="Enter Here"
+							onChange={this.handleChange}
+							value={this.state.default.school}
 						/>
 					</div>
 
@@ -35,7 +73,9 @@ class Education extends Component{
 						<input
 							type="text"
 							name="from"
-							placeholder="YYYY" 
+							placeholder="YYYY"
+							onChange={this.handleChange}
+							value={this.state.default.from}
 						/>
 					</div>
 
@@ -44,7 +84,9 @@ class Education extends Component{
 						<input
 							type="text"
 							name="to"
-							placeholder="YYYY" 
+							placeholder="YYYY"
+							onChange={this.handleChange}
+							value={this.state.default.to}
 						/>
 					</div>
 
@@ -53,7 +95,9 @@ class Education extends Component{
 						<input
 							type="text"
 							name="qualification"
-							placeholder="Enter Here" 
+							placeholder="Enter Here"
+							onChange={this.handleChange}
+							value={this.state.default.qualification} 
 						/>
 					</div>
 
@@ -62,18 +106,20 @@ class Education extends Component{
 						<input
 							type="text"
 							name="grade"
-							placeholder="Enter Here" 
+							placeholder="Enter Here"
+							onChange={this.handleChange}
+							value={this.state.default.grade} 
 						/>
 					</div>
 
 					<div className="form-control buttons">
 						<button className="submit"><i className="fa fa-plus-circle"></i> Save</button>
-						<button className="cancel"><i className="fa fa-ban"></i> Cancel</button>
+						<button className="cancel" onClick={this.handleCancel}><i className="fa fa-ban"></i> Cancel</button>
 					</div>
 
 				</form>
 
-				<button className="add-item"><i className="fa fa-plus-circle"></i> Add</button>
+				<button className={hiddenButton} onClick={this.handleAdd}><i className="fa fa-plus-circle"></i> Add</button>
 			</div>
 		)
 	}
