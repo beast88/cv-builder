86e8739b7fde6d8417708a380fb886540bf2adc8
Paul Rogers <progers999@hotmail.co.uk>
2020-12-07 12:06:37 +0000

    Event handlers for the experience component has been added

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -9,6 +9,7 @@ class Experience extends Component{
 			activeButton: true,
 			jobs: [],
 			default: {
+				id: uniqid(),
 				company: '',
 				city: '',
 				from: '',
@@ -19,22 +20,75 @@ class Experience extends Component{
 		}
 	}
 
+	toggleForm = () => {
+		this.setState({
+			activeForm: !this.state.activeForm,
+			activeButton: !this.state.activeButton
+		})
+	}
+
+	handleAdd = () => {
+		this.toggleForm()
+	}
+
+	handleChange = (e) => {
+		const {name, value} = e.target
+
+		this.setState({
+			default: {...this.state.default, [name]: value}
+		})
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+
+		this.setState({
+			jobs: [...this.state.jobs, this.state.default]
+		})
+
+		this.toggleForm()
+		this.resetForm()
+	}
+
+	handleCancel = (e) => {
+		e.preventDefault()
+		this.toggleForm()
+		this.resetForm()
+	}
+
+	resetForm = () => {
+		this.setState({
+			default: {...this.state.default,
+				id: uniqid(),
+				company: '',
+				city: '',
+				from: '',
+				to: '',
+				role: '',
+				desc: ''
+			}
+		})
+
+	}
+
 	render() {
 
+		let hiddenForm = this.state.activeForm ? "experience-form" : "experience-form hidden"
 		let hiddenButton = this.state.activeButton ? "add-item" : "add-item hidden"
 
 		return(
 			<div className="experience">
 				<h3>Work Experience</h3>
 
-				<form className="experience-form">
+				<form className={hiddenForm} onSubmit={this.handleSubmit}>
 					<div className="form-control">
 						<label for="company">Company:</label>
 						<input
 							type="text"
 							name="company"
 							placeholder="Enter Here"
-							value={this.state.default.company} 
+							value={this.state.default.company}
+							onChange={this.handleChange}
 						/>
 					</div>
 
@@ -44,7 +98,8 @@ class Experience extends Component{
 							type="text"
 							name="city"
 							placeholder="Enter Here"
-							value={this.state.default.city} 
+							value={this.state.default.city}
+							onChange={this.handleChange}
 						/>
 					</div>
 
@@ -54,7 +109,8 @@ class Experience extends Component{
 							type="text"
 							name="from"
 							placeholder="YYYY"
-							value={this.state.default.from} 
+							value={this.state.default.from}
+							onChange={this.handleChange} 
 						/>
 					</div>
 
@@ -64,7 +120,8 @@ class Experience extends Component{
 							type="text"
 							name="to"
 							placeholder="YYYY"
-							value={this.state.default.to} 
+							value={this.state.default.to}
+							onChange={this.handleChange} 
 						/>
 					</div>
 
@@ -74,7 +131,8 @@ class Experience extends Component{
 							type="text"
 							name="role"
 							placeholder="Enter Here"
-							value={this.state.default.role} 
+							value={this.state.default.role}
+							onChange={this.handleChange} 
 						/>
 					</div>
 
@@ -83,18 +141,19 @@ class Experience extends Component{
 						<textarea
 							name="desc"
 							placeholder="Give a quick explanation of your role. Keep it to 2 or 3 sentences."
-							value={this.state.default.desc} 
+							value={this.state.default.desc}
+							onChange={this.handleChange} 
 						/>
 					</div>
 
 					<div className="form-control buttons">
 						<button className="submit"><i className="fa fa-plus-circle"></i> Save</button>
-						<button className="cancel"><i className="fa fa-ban"></i> Cancel</button>
+						<button className="cancel" onClick={this.handleCancel}><i className="fa fa-ban"></i> Cancel</button>
 					</div>
 
 				</form>
 
-				<button className={hiddenButton}><i className="fa fa-plus-circle"></i> Add</button>
+				<button className={hiddenButton} onClick={this.handleAdd}><i className="fa fa-plus-circle"></i> Add</button>
 			</div>
 		)
 	}
