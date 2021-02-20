c4cd77b78a4124ff9d87b726a29de94bfd2ab50d
Paul Rogers <progers999@hotmail.co.uk>
2021-02-20 13:15:50 +0000

    Updates

---
+++ precache-manifest.0ebadea460cb0897a5d23ad4a5fc4638.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3264d78699df6f6c25cb",
+    "url": "/cv-builder/static/js/main.9bd2a14e.chunk.js"
+  },
+  {
+    "revision": "24c14ba04625cb871ed5",
+    "url": "/cv-builder/static/js/runtime~main.7d3b05d7.js"
+  },
+  {
+    "revision": "cafff368b8225ef68579",
+    "url": "/cv-builder/static/js/2.6564fcfa.chunk.js"
+  },
+  {
+    "revision": "c1853e1bec3aae0dd780f1d31e57579a",
+    "url": "/cv-builder/index.html"
+  }
+];
